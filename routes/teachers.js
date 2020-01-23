6b1de6498e8119bb667af958d979e9b433e5879d
yash <yashpk789987@gmail.com>
2020-01-23 06:45:51 +0530

    teacher create show all

--- routes/teachers.js
+++ routes/teachers.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'exam_result_system'
+});
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -7,6 +16,23 @@ router.get('/', function(req, res, next) {
 
 router.get('/create', function(req, res) {
   res.render('teachers/create', { message: '' });
+});
+
+router.post('/create', function(req, res) {
+  pool.query('INSERT INTO teachers SET ?', req.body, function(err, result) {
+    if (err) {
+      throw err;
+    }
+    res.redirect('/teacher/all');
+  });
+});
+
+router.get('/all', function(req, res) {
+  let query = `select * from teachers`;
+  pool.query(query, function(err, result) {
+    if (err) throw err;
+    res.render('teachers/all', { teachers: result });
+  });
 });
 
 module.exports = router;
